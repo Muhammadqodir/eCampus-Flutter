fdbd6e042b2df35d3391f2ce25c45c05a060dfdb
Muhammadqodir <m.qodir777@gmail.com>
2022-08-04 16:34:55 +0500

    Created ScheduleLessonModel

---
+++ lib/models/schedule_model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ScheduleLessonsModel{
+  String subName, room, timeStart, timeEnd, teacher, lessonType, group;
+  int para, teacherId, roomId;
+  bool current;
+
+  ScheduleLessonsModel({
+    required this.subName,
+    required this.room,
+    required this.timeStart,
+    required this.timeEnd,
+    required this.teacher,
+    required this.lessonType,
+    required this.group,
+    required this.para,
+    required this.teacherId,
+    required this.roomId,
+    required this.current
+    });
+
+}
