aa16148a2cc1293f60ffc1b5e902306f8450d81c
Muhammadqodir <m.qodir777@gmail.com>
2022-09-04 18:21:18 +0300

    Added imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ecampus_ncfu/pages/login_page.dart';
 import 'package:ecampus_ncfu/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 
 void main() async {
