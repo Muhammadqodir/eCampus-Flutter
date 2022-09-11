6f32f9d6aeebe50b3c17feebab9953c4f05c6469
Muhammadqodir <m.qodir777@gmail.com>
2022-09-11 18:52:23 +0300

    Created content_epass.dart

---
+++ lib/pages/contents/content_epass.dart
@@ -0,0 +1,36 @@
+import 'package:ecampus_ncfu/ecampus_icons.dart';
+import 'package:ecampus_ncfu/inc/cross_list_element.dart';
+import 'package:ecampus_ncfu/inc/service_item.dart';
+import 'package:ecampus_ncfu/pages/map_page.dart';
+import 'package:ecampus_ncfu/pages/my_teachers_page.dart';
+import 'package:ecampus_ncfu/pages/rating_page.dart';
+import 'package:ecampus_ncfu/pages/statistics_page.dart';
+import 'package:ecampus_ncfu/utils/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+class ContentServices extends StatefulWidget {
+  const ContentServices(
+      {Key? key, required this.context})
+      : super(key: key);
+
+  final BuildContext context;
+
+  @override
+  State<ContentServices> createState() => _ContentServicesState();
+}
+
+class _ContentServicesState extends State<ContentServices> {
+  double elevation = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
